f43d837e344ca68b7dedf51f3b2acda7e158802b
Leo is awesome <lchan1994@yahoo.com>
2015-05-27 12:50:11 -0400

    gotta increase that github score

---
+++ radixR_adder/radixR_adder.srcs/sources_1/new/radixR_adder.vhd
@@ -0,0 +1,68 @@
+
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+entity radixR_adder is
+    generic (
+        r : natural := 2; -- r radix
+        n : natural := 1  -- n is the least integer > log r
+    );
+    port (
+        x, y : in std_logic_vector(n-1 downto 0);
+        ck,btn0,btn1,reset: in std_logic;
+        z : out std_logic_vector(n-1 downto 0)
+    );
+end radixR_adder;
+
+architecture Behavioral of radixR_adder is
+    signal temp_x, temp_y : std_logic_vector(n downto 0) ; -- n+1 bit vector
+
+    -- Loop convert the num in a given base to the decimal reporesentation 
+    function radix_to_int(r: integer; num: integer) return integer is
+        variable i: integer:= 0;
+        variable sum: integer := 0;
+        variable digit: integer;
+        variable num_copy: integer;
+        begin
+        num_copy := num;
+        while num_copy > 0.5 Loop
+            digit := num_copy mod 10;
+            sum := sum + digit * (r**i);
+            num_copy := num_copy/10;
+            i := i + 1;
+        end loop;
+        return sum;
+    end radix_to_int;
+
+    begin
+
+    temp_x <= '0' & x; -- extend operands to
+    temp_y <= '0' & y; -- n+1 bits
+    
+    process (ck)        
+        variable x_temp, y_temp: integer;
+    
+        begin
+        
+        if ck='1' and ck'event then
+            if reset='1' then
+                x_temp := 0;
+                y_temp := 0;
+                z <= std_logic_vector(to_unsigned(0, z'length));
+            else
+                if btn0='1' then 
+                    -- load values for x and y
+                    x_temp := radix_to_int(r, to_integer(unsigned(x)));
+                    y_temp := radix_to_int(r, to_integer(unsigned(y)));
+                elsif btn1='1' then
+                    -- find and display the sum
+                    z <= std_logic_vector(to_unsigned(x_temp+y_temp, z'length));
+                end if;
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
